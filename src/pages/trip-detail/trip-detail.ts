bdbe88f553037a648f94e36f79902f9451ddebab
Unknown <2015isha.shetty@ves.ac.in>
2019-02-09 23:40:06 +0530

    graph added
    graph added

--- src/pages/trip-detail/trip-detail.ts
+++ src/pages/trip-detail/trip-detail.ts
@@ -2,6 +2,8 @@ import {Component} from "@angular/core";
 import {NavController} from "ionic-angular";
 import {TripService} from "../../services/trip-service";
 import {CheckoutTripPage} from "../checkout-trip/checkout-trip";
+import { ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-trip-detail',
@@ -14,12 +16,56 @@ export class TripDetailPage {
   public adults = 2;
   // number of children
   public children = 0;
+  //bar chart drawing
+  @ViewChild('barCanvas') barCanvas;
+
+    barChart: any;
 
   constructor(public nav: NavController, public tripService: TripService) {
     // set sample data
     this.trip = tripService.getItem(1);
   }
+  ionViewDidLoad() {
 
+        this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+            type: 'bar',
+            data: {
+                labels: ["Sem1", "Sem2", "Sem3", "Sem4", "Sem5(predicted)"],
+                datasets: [{
+                    label: 'Semester wise SGPI',
+                    data: [9.3,9.2,8.08, 8.8, 9, 9.4],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        // 'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 0.2
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+
+        });
+
+}
   // minus adult when click minus button
   minusAdult() {
     this.adults--;
