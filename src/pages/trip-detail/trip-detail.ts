8692b40e1068842878438e26f3971ef75b6da1de
Dipshi <2015dipshi.shetty@ves.ac.in>
2019-03-25 22:54:59 +0530

    see prediciton page

--- src/pages/trip-detail/trip-detail.ts
+++ src/pages/trip-detail/trip-detail.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {NavController} from "ionic-angular";
+import {NavController,NavParams} from "ionic-angular";
 import {TripService} from "../../services/trip-service";
 import {CheckoutTripPage} from "../checkout-trip/checkout-trip";
 import { ViewChild } from '@angular/core';
@@ -8,6 +8,8 @@ import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { Response,Http } from '@angular/http';
 import {ApiconnectProvider} from  "../../providers/apiconnect/apiconnect";
+import {RegisterUserProvider} from  "../../providers/register-user/register-user";
+
 
 @Component({
   selector: 'page-trip-detail',
@@ -16,89 +18,66 @@ import {ApiconnectProvider} from  "../../providers/apiconnect/apiconnect";
 export class TripDetailPage {
   // trip info
   public trip: any;
-  // number of adult
-  public adults = 2;
-  // number of children
-  public children = 0;
-  //bar chart drawing
-  //  requests: Observable<any>;
+   value;
+  weathers:any;
+  firstname;
+  fathername;
+  mothername;
+  lastname;
+  branch;
+  Dob;
+  gap;
+  caste;
+  sem1;
+  sem2;
+  sem3;
+  sem4;
+
    public requests:any=[];
   @ViewChild('barCanvas') barCanvas;
     
     barChart: any;
 
-  constructor(public nav: NavController, public tripService: TripService, private msgService: ApiconnectProvider,public httpClient:HttpClient) {
+  constructor(public nav: NavController, public tripService: TripService, public navParams: NavParams,private userReg:RegisterUserProvider,private msgService: ApiconnectProvider,public httpClient:HttpClient) {
     // set sample data
-    this.trip = tripService.getItem(1);
-    // this.getDetails();
+      this.value=navParams.get('id');
+      console.log("current id is ",this.value);    // this.getDetails();
   }
+  ionViewDidLoad() {
+    // console.log('ionViewDidLoad ProfilePage');
+    this.userReg.getUserDetails(this.value).subscribe((weather) => {
+      
+        this.weathers = weather,
+        this.firstname=this.weathers.FirstName,
+        this.branch=this.weathers.Branch,
+        this.mothername=this.weathers.MotherName,
+        this.fathername=this.weathers.FatherName,
+        this.lastname=this.weathers.LastName,
+        this.Dob=this.weathers.Date_of_Birth,
+        this.caste=this.weathers.caste,
+        this.admiss_cat=this.weathers.Admission_Category,
+        this.gap=this.weathers.gap,
+        this.gender=this.weathers.gender,
+        //  this.Sem1=thi
 
+    });
+
+     this.userReg.getEduDetails(this.value).subscribe((weather) => {
+      
+         this.weathers = weather,
+         this.tenth_score=this.weathers.tenth_score,
+         this.twelve_score=this.weathers.twelve_score,
+         this.Sem1=this.weathers.Sem1,
+         this.Sem2=this.weathers.Sem2,
+         this.Sem3=this.weathers.Sem3,
+         this.Sem4=this.weathers.Sem4,
+      
+        //  this.Sem1=thi
+
+    });
   
+
+  } 
        
-//   ionViewDidLoad() {
-
-//         this.barChart = new Chart(this.barCanvas.nativeElement, {
-
-//             type: 'bar',
-//             data: {
-//                 labels: ["Sem1", "Sem2"
-// , "Sem3", "Sem4", "Sem5(predicted)"],
-//                 datasets: [{
-//                     label: 'Semester wise SGPI',
-//                     data: [9.3,9.2,8.08, 8.8, 9, 9.4],
-//                     backgroundColor: [
-//                         'rgba(255, 99, 132, 0.2)',
-//                         'rgba(54, 162, 235, 0.2)',
-//                         'rgba(255, 206, 86, 0.2)',
-//                         'rgba(75, 192, 192, 0.2)',
-//                         'rgba(153, 102, 255, 0.2)',
-//                     ],
-//                     borderColor: [
-//                         'rgba(255,99,132,1)',
-//                         'rgba(54, 162, 235, 1)',
-//                         'rgba(255, 206, 86, 1)',
-//                         'rgba(75, 192, 192, 1)',
-//                         'rgba(153, 102, 255, 1)',
-//                         // 'rgba(255, 159, 64, 1)'
-//                     ],
-//                     borderWidth: 0.2
-//                 }]
-//             },
-//             options: {
-//                 scales: {
-//                     yAxes: [{
-//                         ticks: {
-//                             beginAtZero:true
-//                         }
-//                     }]
-//                 }
-//             }
-
-//         });
-
-// }
-  // minus adult when click minus button
-  minusAdult() {
-    this.adults--;
-  }
-
-  // plus adult when click plus button
-  plusAdult() {
-    this.adults++;
-  }
-
-  // minus children when click minus button
-  minusChildren() {
-    this.children--;
-  }
-
-  // plus children when click plus button
-  plusChildren() {
-    this.children++;
-  }
-
-  // go to checkout page
-  checkout() {
-    this.nav.push(CheckoutTripPage);
-  }
+  
 }
